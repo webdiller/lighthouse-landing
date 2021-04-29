b74964f313074bb645af03ce26aa9ace7fcba2b4
eugene <eugenefromrus@gmail.com>
2021-04-29 16:51:23 +1000

    Ready desktop version

---
+++ index.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const d = document;
+  const obj1 = d.getElementById("obj1");
+  const obj2 = d.getElementById("obj2");
+  const obj3 = d.getElementById("obj3");
+  const obj4 = d.getElementById("obj4");
+  const obj5 = d.getElementById("obj5");
+  const obj6 = d.getElementById("obj6");
+  const obj7 = d.getElementById("obj7");
+  const obj8 = d.getElementById("obj8");
+  const obj9 = d.getElementById("obj9");
+  const obj10 = d.getElementById("obj10");
+  const obj11 = d.getElementById("obj11");
+  const obj12 = d.getElementById("obj12");
+  const obj13 = d.getElementById("obj13");
+  const obj14 = d.getElementById("obj14");
+
+  let arr = [
+    { name: obj1, type: "up", delay: 2 },
+    { name: obj2, type: "down", delay: 2 },
+    { name: obj3, type: "up", delay: 2 },
+    { name: obj4, type: "down", delay: 2 },
+    { name: obj5, type: "up", delay: 2 },
+    { name: obj6, type: "down", delay: 4 },
+    { name: obj7, type: "up", delay: 3 },
+    { name: obj8, type: "down", delay: 3 },
+    { name: obj9, type: "up", delay: 3 },
+    { name: obj10, type: "down", delay: 1 },
+    { name: obj11, type: "up", delay: 2 },
+    { name: obj12, type: "down", delay: .4 },
+    { name: obj13, type: "up", delay: 6 },
+    { name: obj14, type: "down", delay: 5 }
+  ];
+
+  generetaParalax(arr);
+
+  function generetaParalax(elements) {
+    elements.forEach(item => {
+      new simpleParallax(item.name, {
+        delay: item.delay,
+        transition: "cubic-bezier(0,0,0,1)",
+        overflow: true,
+        orientation: item.type
+      });
+    });
+  }
+
+  (function () {
+    const allQuestions = d.getElementById("allQuestions");
+    const faqContainer = d.getElementById("faqContainer");
+    const questions = d.querySelectorAll("#questions .faq__question-title");
+    if (questions && faqContainer) {
+      questions.forEach(item => {
+        item.addEventListener("click", function (e) {
+          item.parentElement.classList.toggle("active");
+          faqContainer.classList.add("active");
+        });
+      });
+    }
+    if (allQuestions) {
+      allQuestions.addEventListener("click", function () {
+        this.remove();
+        d.querySelectorAll("#questions .faq__question").forEach(item => item.classList.add("active"));
+        document.querySelectorAll(".faq__question-show-all ~ .faq__question").forEach(item => item.classList.add("active"));
+        faqContainer.classList.add("active");
+      });
+    }
+  })();
+});
